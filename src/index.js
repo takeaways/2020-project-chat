2a2e799166f5301a0cc6e8583340840c64e8c155
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-11-30 23:25:31 +0900

    Add Redux

--- src/index.js
+++ src/index.js
@@ -5,10 +5,27 @@ import App from './App.jsx'
 import reportWebVitals from './reportWebVitals'
 import GlobalStyles from './commons/components/GlobalStyles'
 
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
+import promiseMiddleware from 'redux-promise'
+import thunk from 'redux-thunk'
+
+import rootReducer from './redux/reducer'
+
+const createStoreWidthMiddleware = applyMiddleware(promiseMiddleware, thunk)(createStore)
+
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
-    <App />
+    <Provider
+      store={createStoreWidthMiddleware(
+        rootReducer, //
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+      )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
