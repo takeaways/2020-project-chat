afe2c2cb15dbfb02ab634d0060348dabec50307e
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-12-01 00:33:42 +0900

    Add register user

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,8 +1,8 @@
 import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
-// import firebase from '../../firebase'
-// import md5 from 'md5'
+import firebase from '../../firebase'
+import md5 from 'md5'
 
 function RegisterPage() {
   const { register, watch, errors, handleSubmit } = useForm()
@@ -15,8 +15,10 @@ function RegisterPage() {
   const onSubmit = async (data) => {
     try {
       setLoading(true)
-      //   let createdUser = await firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
-      //   console.log('createdUser', createdUser)
+      let createdUser = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(data.email, data.password)
+      console.log('createdUser', createdUser)
 
       //   await createdUser.user.updateProfile({
       //     displayName: data.name,
@@ -46,18 +48,34 @@ function RegisterPage() {
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>Email</label>
-        <input name="email" type="email" ref={register({ required: true, pattern: /^\S+@\S+$/i })} />
+        <input
+          name="email"
+          type="email"
+          ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+        />
         {errors.email && <p>This email field is required</p>}
 
         <label>Name</label>
         <input name="name" ref={register({ required: true, maxLength: 10 })} />
-        {errors.name && errors.name.type === 'required' && <p>This name field is required</p>}
-        {errors.name && errors.name.type === 'maxLength' && <p>Your input exceed maximum length</p>}
+        {errors.name && errors.name.type === 'required' && (
+          <p>This name field is required</p>
+        )}
+        {errors.name && errors.name.type === 'maxLength' && (
+          <p>Your input exceed maximum length</p>
+        )}
 
         <label>Password</label>
-        <input name="password" type="password" ref={register({ required: true, minLength: 6 })} />
-        {errors.password && errors.password.type === 'required' && <p>This password field is required</p>}
-        {errors.password && errors.password.type === 'minLength' && <p>Password must have at least 6 characters</p>}
+        <input
+          name="password"
+          type="password"
+          ref={register({ required: true, minLength: 6 })}
+        />
+        {errors.password && errors.password.type === 'required' && (
+          <p>This password field is required</p>
+        )}
+        {errors.password && errors.password.type === 'minLength' && (
+          <p>Password must have at least 6 characters</p>
+        )}
 
         <label>Password Confirm</label>
         <input
@@ -68,14 +86,18 @@ function RegisterPage() {
             validate: (value) => value === password.current,
           })}
         />
-        {errors.password_confirm && errors.password_confirm.type === 'required' && (
-          <p>This password confirm field is required</p>
-        )}
-        {errors.password_confirm && errors.password_confirm.type === 'validate' && <p>The passwords do not match</p>}
+        {errors.password_confirm &&
+          errors.password_confirm.type === 'required' && (
+            <p>This password confirm field is required</p>
+          )}
+        {errors.password_confirm &&
+          errors.password_confirm.type === 'validate' && (
+            <p>The passwords do not match</p>
+          )}
 
         {errorFromSubmit && <p>{errorFromSubmit}</p>}
 
-        <input type="submit" disabled={loading} />
+        <input type="submit" disabled={loading} value="SUBMIT" />
         <Link style={{ color: 'gray', textDecoration: 'none' }} to="login">
           이미 아이디가 있다면...{' '}
         </Link>
--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,17 @@ import 'firebase/database'
 import 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASEURL,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MEASUREMENTID,
+  apiKey: 'AIzaSyBhQxrc8h42vmmEVVQ7cp_gWmWYlYOTo7A',
+  authDomain: 'react-firebase-chat-app-ebacd.firebaseapp.com',
+  databaseURL: 'https://react-firebase-chat-app-ebacd.firebaseio.com',
+  projectId: 'react-firebase-chat-app-ebacd',
+  storageBucket: 'react-firebase-chat-app-ebacd.appspot.com',
+  messagingSenderId: '804665050980',
+  appId: '1:804665050980:web:a6fdabf023a822f98f1c71',
+  measurementId: 'G-W8SJVKP78T',
 }
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 firebase.analytics()
+
+export default firebase
