241f249ac1c6b419e107ce8f2fdbc17636f350d0
JangGeonil <wkdrjsdlf2@gmail.com>
2020-12-02 00:48:03 +0900

    Add Message Form

--- src/components/ChatPage/MainPanel/MessageForm.jsx
+++ src/components/ChatPage/MainPanel/MessageForm.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import Form from 'react-bootstrap/Form'
+import ProgressBar from 'react-bootstrap/ProgressBar'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 function MessageForm() {
-  return <div>MessageForm</div>
+  return (
+    <Wrapper>
+      <Form>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
+      <ProgressBar variant="warning" now={60} label={`60%`} />
+      <Row>
+        <Col>
+          <button>SEND</button>
+        </Col>
+        <Col>
+          <button>UPLOAD</button>
+        </Col>
+      </Row>
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.div`
+  button {
+    width: 100%;
+    background: #7a84eb;
+
+    color: white;
+    text-transform: uppercase;
+    border: none;
+    margin-top: 40px;
+    padding: 20px;
+    font-size: 16px;
+    font-weight: 100;
+    letter-spacing: 10px;
+  }
+  button:hover {
+    background: #636ee6;
+  }
+`
 
 export default MessageForm
